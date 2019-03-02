6c17565a3f9404d6eb0c0dcfac01399206badccd
Lupe <lupec_se@yahoo.com>
2019-03-02 13:57:48 -0700

    register.js changes

--- project3/client/src/components/Register/Register.js
+++ project3/client/src/components/Register/Register.js
@@ -23,24 +23,24 @@ class Register extends React.Component {
     this.setState({password: event.target.value})
   }
 
-  // onSubmitSignIn = () => {
-  //   fetch('http://localhost:3000/register', {
-  //     method: 'post',
-  //     headers: {'Content-Type': 'application/json'},
-  //     body: JSON.stringify({
-  //       email: this.state.email,
-  //       password: this.state.password,
-  //       name: this.state.name
-  //     })
-  //   })
-  //     .then(response => response.json())
-  //     .then(user => {
-  //       if (user.id) {
-  //         this.props.loadUser(user)
-  //         this.props.onRouteChange('home');
-  //       }
-  //     })
-  // }
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/register', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
+  }
 
   render() {
     return (
