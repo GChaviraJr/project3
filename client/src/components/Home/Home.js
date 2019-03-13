f4ba2f39aabc7fc85709512019ed69e9a409e204
Lupe <lupec_se@yahoo.com>
2019-03-12 18:24:44 -0700

    Chaned Home into react component instead of func

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, welcome to your home page`}
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+    render() {
+      const { name, entries } = this.props
+    return (
+      <div>
+        <div className='white f3'>
+          {`${name}, welcome to your home page`}
+        </div>
+        <div className='white f1'>
+          {entries}
+        </div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+      )
+    }
 }
 
 export default Home;
