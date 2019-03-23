d3dd6aa82b4ee321846556c1f0f0ea8658e69e57
Hailey Dean <haileydean@MacBook-Pro.local>
2019-03-23 12:02:12 -0700

    yelp component

--- client/src/components/Yelp/Yelp.js
+++ client/src/components/Yelp/Yelp.js
@@ -65,16 +65,24 @@ var refreshRestaurants = function () {
     $userCityInput.val("");
   };
 
-  render(){
-      {/* const {
-          name of variable (name)
-      } = this.props */}
+  render() {
+      const { name } = this.props
       return (
-          <div>  
-              {/* All html needs to be in here */}
-          </div>
+          <div>
+        <div className='white f3'>
+          <h2>{`${name}, welcome to your home page`}</h2>
+        </div>
+        <div className="white br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">Where do we want to go?</div>
+        <div className="white br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <label className='mt2 fw6' htmlFor='yelp-search'>Search for places to go</label>
+            <input onSearchChange={this.search} type='text' name='yelp-search' className='pa2 ba w-100' placeholder='bar, club...'></input>
+        </div>
+        <div className="white br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">{`We are going to ${this.search}`}</div>
+        <div className="white br3 ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">If you need a ride, use Uber!</div>
+        <button>Uber Button</button>
+      </div>
       )
-  }
-}
+  };
+};
 
 export default Yelp;
